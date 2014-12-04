6e553f2f90a1a64fa5308057cabc08505865559c
Joakim Granbohm <joakim.granbohm@frantic.com>
2014-12-04 08:14:31 +0000

    Header: create header and add login box toggle functionalities

---
+++ src/app/components/header.directive.js
@@ -0,0 +1,37 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('dcHeader', dcHeader);
+
+    dcHeader.$inject = ['$location'];
+
+    /* @ngInject */
+    function dcHeader($location) {
+        // Usage:
+        //      Add element <dc-header></dc-header>
+        // Creates:
+        //      Element that has the contents of header.directive.html
+        var directive = {
+            link: link,
+            templateUrl: 'app/components/header.directive.html',
+            restrict: 'EA',
+            controller: Header,
+            controllerAs: 'header'
+        };
+        return directive;
+
+        function Header() {
+        	var vm = this;
+            vm.displayLogin = false;
+
+            vm.toggleLogin = function(){
+                vm.displayLogin = !vm.displayLogin;
+            }
+        }
+
+        function link(scope, element, attrs) {
+        }
+    }
+})();
