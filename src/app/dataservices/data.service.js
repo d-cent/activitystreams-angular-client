c4258b0867935b581d4dec1710a8122518e56c32
Joakim Granbohm <joakim.granbohm@frantic.com>
2014-12-04 08:18:21 +0000

    Services: data.service serves different dummydata for frontpage and feed page

--- src/app/dataservices/data.service.js
+++ src/app/dataservices/data.service.js
@@ -8,10 +8,12 @@
 	dataService.$inject = ['$resource'];
 
 	function dataService($resource){
-		var postsPath = "../jsondata/feed.json"
+		var feed = "../jsondata/feed.json"
+		var homeFeed = "../jsondata/home-feed.json"
 		
 		return {
-			posts: $resource(postsPath),
+			posts: $resource(homeFeed),
+			feed: $resource(feed)
 		}
 	}
 
